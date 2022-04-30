d95d318b0a0b50aca4a59afbd418428e3fea4079
unknown <andresfelipehc@ufps.edu.co>
2022-04-30 08:32:34 -0500

    Haciendo paginas estaticas como respuestas de rutas

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-app.get('/',  (req, res) => {
-  res.send('Home page')
-})
+//Servir contenido estatico
+app.use(express.static('public'));
+
 
 app.get('/hola-mundo',  (req, res) =>{
     res.send('Hello World')
   })
+
+  app.get('*', (req, res)=>{
+    res.sendFile(__dirname +'/public/404.html');
+  });
 
 app.listen(port,()=>{
     console.log(`Aplicacion corriendo en el puerto ${port}`);
