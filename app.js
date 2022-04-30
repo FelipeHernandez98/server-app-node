f9b577ff4c2310e5e546b0eef76b5ee0756d6b09
unknown <andresfelipehc@ufps.edu.co>
2022-04-30 08:57:26 -0500

    Agregando un template gratuito como ejemplo de envio de archivos html

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const port = 3000;
 //Servir contenido estatico
 app.use(express.static('public'));
 
+  app.get('/generic', (req, res)=>{
+    res.sendFile(__dirname +'/public/generic.html');
+  });
 
-app.get('/hola-mundo',  (req, res) =>{
-    res.send('Hello World')
-  })
+  app.get('/elements', (req, res)=>{
+    res.sendFile(__dirname +'/public/elements.html');
+  });
 
   app.get('*', (req, res)=>{
     res.sendFile(__dirname +'/public/404.html');
