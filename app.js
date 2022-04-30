22a912ab6118ae88173ca5594280ec41a78b9ada
unknown <andresfelipehc@ufps.edu.co>
2022-04-30 10:02:10 -0500

    Agregando partials con handlebars para reutilizar codigo

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
 const port = 3000;
 
-
+//Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 //Servir contenido estatico
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    nombre: 'Felipe Hernandez',
+    titulo: 'Practica de node'
+  });
 });
 
+
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    nombre: 'Felipe Hernandez',
+    titulo: 'Practica de node | generic'
+  });
 });
 
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    nombre: 'Felipe Hernandez',
+    titulo: 'Practica de node | elements'
+  });
 });
 
 app.get('*', (req, res) => {
